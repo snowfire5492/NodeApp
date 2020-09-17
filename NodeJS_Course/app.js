c90c3d9d9cdf1d2d55014a6c7ed0eb7af7f239ee
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-09-16 21:54:18 -0700

    fix controllers

--- NodeJS_Course/app.js
+++ NodeJS_Course/app.js
@@ -1,38 +1,24 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express() 
+// const errorController = require('./controllers/error');
 
+const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.set('view engine', 'ejs')
-app.set('views', 'views') // unnecessary as views is the default for this
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const adminRoutes = require('./routes/admin')   // valid middleware function 
-const shopRoutes = require('./routes/shop')
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
+// app.use(errorController.get404);
 
-app.use(bodyParser.urlencoded({extended: false}))   
-
-app.use(express.static(path.join(__dirname, 'public')))     // used so we can access css files in public path
-
-app.use('/admin', adminRoutes.routes)    // so use the middle ware. (OREDER MATTERS)
-app.use(shopRoutes)     // order matters here. shop should be second. It would work other way but only because we have get inside shop
-                        // if we had use then it would not work if we switched the order here.  
-
-
-// to make a 404 request take advantage of middleware 
-// if we dont hit the page thru our middleware we end 
-// up down at the bottom here
-
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-    res.render('404', {pageTitle: 'Page Not Found'})
-})
-
-
-
-app.listen(3000)
+app.listen(3000);
--- NodeJS_Course/routes/shop.js
+++ NodeJS_Course/routes/shop.js
@@ -1,29 +1,11 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express') 
+const express = require('express');
 
-const rootDir = require('../util/path')
-const adminData = require('./admin')
+const productsController = require('../controllers/products');
 
+const router = express.Router();
 
-const router = express.Router()
+router.get('/', productsController.getProducts);
 
-
-
-// use would be okay to but we only want to hanlde get requests here
-// GET will do an exact match for '/'
-router.get('/', (req, res, next) => {
-    const products = adminData.products
-    res.render('shop', { 
-        products: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-     })
-})
-
-
-
-module.exports = router
+module.exports = router;
