0cc78af09655e09546503e63ab69ae118654434d
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-09-16 21:31:46 -0700

    finalize ejs templates for views

--- NodeJS_Course/app.js
+++ NodeJS_Course/app.js
@@ -2,15 +2,13 @@ const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const expressHbs = require('express-handlebars')
 
 const app = express() 
 
 
 
-app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}))
 
-app.set('view engine', 'handlebars')
+app.set('view engine', 'ejs')
 app.set('views', 'views') // unnecessary as views is the default for this
 
 const adminData = require('./routes/admin')   // valid middleware function 
