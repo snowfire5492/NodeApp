e4ae581873e50a8121a50fd533167924b80b30c6
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-09-21 12:50:26 -0700

    update app

--- NodeJS_Course/models/product.js
+++ NodeJS_Course/models/product.js
@@ -1,24 +1,40 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-        )
-        fs.readFile(p, (err, fileContent) => {
-            console.log(fileContent)
-        })
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    // using static keyword to make sure we call fetchAll directly on the 
-    // class itself AND NOT ON an instantiated object! 
-    static fetchAll() {
-        return products
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
--- NodeJS_Course/util/path.js
+++ NodeJS_Course/util/path.js
@@ -1,5 +1,3 @@
-const path = require('path')
+const path = require('path');
 
-// refers to mainModule we started the app in
-// gives us path to file that makes our app run
-module.exports = path.dirname(process.mainModule.filename)
+module.exports = path.dirname(process.mainModule.filename);
