d38996bd3ee5137c6da4299abd8960303ecd797c
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-10-09 03:30:48 -0700

    mongoose

--- NodeJS_Course/app.js
+++ NodeJS_Course/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error');
 // const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -19,20 +19,20 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5f7eee7f229cb58a40e4117c')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5f7eee7f229cb58a40e4117c')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-mongoose.connect('mongodb+srv://admin:admin1234@mymongodb.tcwcj.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://admin:admin1234@mymongodb.tcwcj.mongodb.net/shop?retryWrites=true&w=majority')
     .then(result => {
         app.listen(3000)
     })
