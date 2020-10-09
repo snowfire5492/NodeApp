0917fb68c549af9945a1f8b04cbf67cdca2d9128
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-10-09 06:38:26 -0700

    admin log in

--- NodeJS_Course/controllers/auth.js
+++ NodeJS_Course/controllers/auth.js
@@ -1,7 +1,20 @@
+
+
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req
+        .get('Cookie')
+        .split('=')[1]
+    console.log(isLoggedIn)
     res.render('auth/login', {
       path: '/login',
-      pageTitle: 'Login'
+      pageTitle: 'Login',
+      isAuthenticated: req.isLoggedIn
     });
+  };
+
+
+  exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true')
+    res.redirect('/')
   };
   
