b304229db502e4dd47b7c29a2545da17c20a17e7
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-10-10 00:58:28 -0700

    install session middleware

--- NodeJS_Course/controllers/auth.js
+++ NodeJS_Course/controllers/auth.js
@@ -3,18 +3,18 @@
 exports.getLogin = (req, res, next) => {
     const isLoggedIn = req
         .get('Cookie')
-        .split('=')[1]
+        .split('=')[1] === 'true'
     console.log(isLoggedIn)
     res.render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
-      isAuthenticated: req.isLoggedIn
+      isAuthenticated: isLoggedIn
     });
   };
 
 
   exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true')
+    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
     res.redirect('/')
   };
   
