9b2ed685f795bafd1f97d9faa3cfe2b88d5c217b
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-10-13 04:03:31 -0700

    upload images

--- NodeJS_Course/app.js
+++ NodeJS_Course/app.js
@@ -9,6 +9,7 @@
   const MongoDBStore = require('connect-mongodb-session')(session);
   const csrf = require('csurf');
   const flash = require('connect-flash');
+  const multer = require('multer')
   
   const errorController = require('./controllers/error');
   const User = require('./models/user');
@@ -22,6 +23,26 @@ const MONGODB_URI = 'mongodb+srv://admin:admin1234@mymongodb.tcwcj.mongodb.net/s
     collection: 'sessions'
   });
   const csrfProtection = csrf();
+
+  const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+      cb(null, new Date().toISOString() + '-' + file.originalname)
+    }
+  })
+
+  const fileFilter =(req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg'
+      || file.mimetype === 'image/jpeg') {
+        cb(null, true)
+    } else {
+      cb(null, false)
+    }
+    
+    
+  }
   
   app.set('view engine', 'ejs');
   app.set('views', 'views');
@@ -31,6 +52,7 @@ const MONGODB_URI = 'mongodb+srv://admin:admin1234@mymongodb.tcwcj.mongodb.net/s
   const authRoutes = require('./routes/auth');
   
   app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(
     session({
